d9b9dc99893d857fbc815342536c72c96e97f45e
Peng Chao <1377318286@qq.com>
2024-02-14 20:06:17 +0800

    add memory save dwpose proc

--- tools/extract_dwpose_from_vid.py
+++ tools/extract_dwpose_from_vid.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 import concurrent.futures
 import os
 import random
 from pathlib import Path
+from PIL import Image
+import av
 
 import numpy as np
 
@@ -10,6 +13,50 @@ from src.utils.util import get_fps, read_frames, save_videos_from_pil
 
 # Extract dwpose mp4 videos from raw videos
 # /path/to/video_dataset/*/*.mp4 -> /path/to/video_dataset_dwpose/*/*.mp4
+
+
+def process_single_video_mem(video_path, detector, root_dir, save_dir):
+    relative_path = os.path.relpath(video_path, root_dir)
+    print(relative_path, video_path, root_dir)
+    out_path = os.path.join(save_dir, relative_path)
+    if os.path.exists(out_path):
+        return
+
+    output_dir = Path(os.path.dirname(os.path.join(save_dir, relative_path)))
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    # load input stream
+    fps = get_fps(video_path)
+    container_in = av.open(video_path)
+    stream_in = next(s for s in container_in.streams if s.type == "video")
+    width, height = stream_in.width, stream_in.height
+
+    # create output stream
+    save_fmt = Path(out_path).suffix
+    assert save_fmt == ".mp4"
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    codec = "libx264"
+    container_out = av.open(out_path, "w")
+    stream_out = container_out.add_stream(codec, rate=fps)
+    stream_out.width = width
+    stream_out.height = height
+
+    for packet in container_in.demux(stream_in):
+        for frame in packet.decode():
+            image = Image.frombytes(
+                "RGB",
+                (frame.width, frame.height),
+                frame.to_rgb().to_ndarray(),
+            )
+            result, score = detector(image)
+            score = np.mean(score, axis=-1)
+
+            av_frame = av.VideoFrame.from_image(result)
+            container_out.mux(stream_out.encode(av_frame))
+
+    container_out.mux(stream_out.encode())
+    container_out.close()
 
 
 def process_single_video(video_path, detector, root_dir, save_dir):
@@ -38,7 +85,7 @@ def process_single_video(video_path, detector, root_dir, save_dir):
 def process_batch_videos(video_list, detector, root_dir, save_dir):
     for i, video_path in enumerate(video_list):
         print(f"Process {i}/{len(video_list)} video")
-        process_single_video(video_path, detector, root_dir, save_dir)
+        process_single_video_mem(video_path, detector, root_dir, save_dir)
 
 
 if __name__ == "__main__":
